51d7d142dbb6937474de74e49c84e8c6238fe096
Dmitriy <dmitriy.lisay@gmail.com>
2020-06-12 11:24:31 +0300

    scripts added

---
+++ sql/postgres_db_init.sql
@@ -0,0 +1,59 @@
+DROP DATABASE IF EXISTS full_stack_ecommerce;
+
+CREATE DATABASE full_stack_ecommerce;
+
+\c full_stack_ecommerce;
+
+CREATE TABLE IF NOT EXISTS product_category (
+	id SERIAL,
+	category_name VARCHAR(255) NULL DEFAULT NULL,
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS product (
+	id SERIAL,
+	sku VARCHAR(255) DEFAULT NULL,
+	name VARCHAR(255) DEFAULT NULL,
+	description VARCHAR(255) DEFAULT NULL,
+	unit_price DECIMAL(13,2) DEFAULT NULL,
+	image_url VARCHAR(255) DEFAULT NULL,
+	active INT DEFAULT 1,
+	units_in_stock INT DEFAULT NULL,
+	date_created DATE DEFAULT NULL,
+	last_updated DATE DEFAULT NULL,
+	category_id INT NOT NULL,
+	PRIMARY KEY (id),
+	CONSTRAINT fk_category FOREIGN KEY (category_id) REFERENCES product_category (id)
+);
+
+INSERT INTO PRODUCT_CATEGORY(CATEGORY_NAME) VALUES ('BOOKS');
+
+INSERT INTO PRODUCT (SKU, NAME, DESCRIPTION, IMAGE_URL, ACTIVE, UNITS_IN_STOCK,
+UNIT_PRICE, CATEGORY_ID,DATE_CREATED)
+VALUES ('BOOK-TECH-1000', 'JavaScript - The Fun Parts', 'Learn JavaScript',
+'assets/images/products/placeholder.png'
+,1,100,19.99,1, NOW());
+
+INSERT INTO PRODUCT (SKU, NAME, DESCRIPTION, IMAGE_URL, ACTIVE, UNITS_IN_STOCK,
+UNIT_PRICE, CATEGORY_ID, DATE_CREATED)
+VALUES ('BOOK-TECH-1001', 'Spring Framework Tutorial', 'Learn Spring',
+'assets/images/products/placeholder.png'
+,1,100,29.99,1, NOW());
+
+INSERT INTO PRODUCT (SKU, NAME, DESCRIPTION, IMAGE_URL, ACTIVE, UNITS_IN_STOCK,
+UNIT_PRICE, CATEGORY_ID, DATE_CREATED)
+VALUES ('BOOK-TECH-1002', 'Kubernetes - Deploying Containers', 'Learn Kubernetes',
+'assets/images/products/placeholder.png'
+,1,100,24.99,1, NOW());
+
+INSERT INTO PRODUCT (SKU, NAME, DESCRIPTION, IMAGE_URL, ACTIVE, UNITS_IN_STOCK,
+UNIT_PRICE, CATEGORY_ID, DATE_CREATED)
+VALUES ('BOOK-TECH-1003', 'Internet of Things (IoT) - Getting Started', 'Learn IoT',
+'assets/images/products/placeholder.png'
+,1,100,29.99,1, NOW());
+
+INSERT INTO PRODUCT (SKU, NAME, DESCRIPTION, IMAGE_URL, ACTIVE, UNITS_IN_STOCK,
+UNIT_PRICE, CATEGORY_ID, DATE_CREATED)
+VALUES ('BOOK-TECH-1004', 'The Go Programming Language: A to Z', 'Learn Go',
+'assets/images/products/placeholder.png'
+,1,100,24.99,1, NOW());
